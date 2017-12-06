4c02ad7d71955b087cd084c8156cfef1ea05e993
Lucas Moraes <lucasvs.moraes@hotmail.com>
2017-12-06 20:57:26 -0200

    Added some stuffs

--- StanfordClassGame/Model/Concentration.swift
+++ StanfordClassGame/Model/Concentration.swift
@@ -10,11 +10,31 @@ import Foundation
 
 class Concentration {
     
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     init(numberOfPairOfCards: Int) {
+        assert(numberOfPairOfCards > 0, "Concentration.init(\(numberOfPairOfCards): You must have at least one pair if cards") //Conditional Error
         for _ in 0..<numberOfPairOfCards {
             let card = Card()
             cards += [card,card] //Classic 'append'
@@ -22,6 +42,7 @@ class Concentration {
     }
     
     func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index): Chosen index is not in the cards") //Conditional Error
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
                 if cards[matchIndex].identifier == cards[index].identifier {
--- StanfordClassGame/ViewController.swift
+++ StanfordClassGame/ViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var cardButtons: [UIButton]!
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private var cardButtons: [UIButton]!
+    @IBOutlet weak private var flipCountLabel: UILabel!
     
-    lazy var game = Concentration(numberOfPairOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game = Concentration(numberOfPairOfCards: numberOfPairOfCards)
     
-    var flipCount: Int = 0 {didSet {self.flipCountLabel.text = "Flips: \(flipCount)"}}
+    var numberOfPairOfCards: Int {
+        return cardButtons.count + 1 / 2
+    }
     
+    private(set) var flipCount: Int = 0 { didSet { self.flipCountLabel.text = "Flips: \(flipCount)" } }
     
     override func viewDidLoad() {super.viewDidLoad()}
 
-    @IBAction func flipFunc(_ sender: UIButton) {
+    @IBAction private func flipFunc(_ sender: UIButton) {
         let cardNumber = cardButtons.index(of: sender)!
         game.chooseCard(at: cardNumber)
         updateViewFromModel()
         flipCount += 1
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -42,22 +45,53 @@ class ViewController: UIViewController {
         }
     }
     
-    var emojiChoices = ["👻","☠️","👾","🧟‍♂️","🌝","🔞","📱","🎮"]
-    var emoji = [Int:String]() // Dictionary
+    private var emojiChoices = ["👻","☠️","👾","🧟‍♂️","🌝","🔞","📱","🎮"]
+    private var emoji = [Int:String]() // Dictionary
     
-    func emoji(for card: Card) -> String {
-
+    private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil , emojiChoices.count > 0 {
-                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+                emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
             }
-            
-        
-        
         return emoji[card.identifier] ?? "?"
     }
-
+    
+    
+    
+    
 }
+
+
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self))) // self = .variable
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
